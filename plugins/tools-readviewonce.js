72711cddad757f8310f0d50db314dbdba524ee88
r0lith <96643526+r0lith@users.noreply.github.com>
2025-04-25 12:40:47 +0530

    Update tools-readviewonce.js

--- plugins/tools-readviewonce.js
+++ plugins/tools-readviewonce.js
@@ -1,15 +1,27 @@
-let { downloadContentFromMessage } = await import('@whiskeysockets/baileys')
+let { downloadContentFromMessage } = await import('@whiskeysockets/baileys');
 
 var handler = async (m, { conn }) => {
-  if (!/viewOnce/.test(m.quoted?.mtype)) throw '✳️❇️ Its Not a ViewOnce Message'
-  let mtype = Object.keys(m.quoted.message)[0]
-  let buffer = await m.quoted.download()
-  let caption = m.quoted.message[mtype].caption || ''
-  conn.sendMessage(m.chat, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m })
-}
+  try {
+    if (!m.quoted) throw '✳️❇️ No quoted message found!';
+    if (!/viewOnce/.test(m.quoted.mtype)) throw '✳️❇️ It\'s not a ViewOnce message!';
 
-handler.help = ['readvo']
-handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'vv', 'readvo']
+    let mtype = Object.keys(m.quoted.message)[0];
+    let buffer = await m.quoted.download();
+    let caption = m.quoted.message[mtype]?.caption || '';
 
-export default handler
+    await conn.sendMessage(
+      m.chat,
+      { [mtype.replace(/Message/, '')]: buffer, caption },
+      { quoted: m }
+    );
+  } catch (err) {
+    console.error('Error processing ViewOnce message:', err);
+    throw err; // Re-throw the error to notify the user
+  }
+};
+
+handler.help = ['readvo'];
+handler.tags = ['tools'];
+handler.command = ['readviewonce', 'read', 'vv', 'readvo'];
+
+export default handler;
