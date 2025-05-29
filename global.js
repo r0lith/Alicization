68a049862bcac12d30e361f9a273b0959a2018ad
r0lith <96643526+r0lith@users.noreply.github.com>
2025-05-29 19:45:03 +0530

    hm

--- global.js
+++ global.js
@@ -2,7 +2,7 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js'
 
 import dotenv from 'dotenv'
-import { existsSync, readFileSync, readdirSync, unlinkSync, watch } from 'fs'
+import { readdirSync, unlinkSync, watch } from 'fs'
 import { createRequire } from 'module'
 import path, { join } from 'path'
 import { platform } from 'process'
@@ -42,8 +42,6 @@ const {
   useMultiFileAuthState,
   MessageRetryMap,
   fetchLatestWaWebVersion,
-  makeCacheableSignalKeyStore,
-  makeInMemoryStore,
   Browsers,
   proto,
   delay,
@@ -85,13 +83,6 @@ const MAIN_LOGGER = pino({ timestamp: () => `,"time":"${new Date().toJSON()}"` }
 
 const logger = MAIN_LOGGER.child({})
 logger.level = 'fatal'
-
-const store = useStore ? makeInMemoryStore({ logger }) : undefined
-store?.readFromFile('./session.json')
-
-setInterval(() => {
-  store?.writeToFile('./session.json')
-}, 10000 * 6)
 
 const msgRetryCounterCache = new NodeCache()
 
@@ -198,8 +189,7 @@ const connectionOptions = {
   generateHighQualityLinkPreview: true,
   getMessage: async key => {
     let jid = jidNormalizedUser(key.remoteJid)
-    let msg = await store.loadMessage(jid, key.id)
-    return msg?.message || ''
+    return ''
   },
   patchMessageBeforeSending: message => {
     const requiresPatch = !!(
@@ -230,7 +220,7 @@ const connectionOptions = {
 
 global.conn = makeWASocket(connectionOptions)
 conn.isInit = false
-store?.bind(conn.ev)
+// store?.bind(conn.ev)
 
 if (pairingCode && !conn.authState.creds.registered) {
   let phoneNumber
